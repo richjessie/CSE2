229ff8325f751bc95071068205df35214c2e745c
richjessie <rja218@lehigh.edu>
2014-09-02 04:26:35 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+
+//define the class 
+public class WelcomeClass{
+//   add main method
+   public static void main(String [ ] args) {
+    
+//   print the statement with box around it
+     System.out.println("-------------");
+     System.out.println(" | WELCOME |");
+     System.out.println("-------------");
+     System.out.println("^   ^   ^   ^   ^   ^ ");
+     System.out.println("/ \\ / \\ / \\ / \\ / \\ / \\");
+     System.out.println("|-R --J --A --2 --1 --8-|");
+     System.out.print("I was born in the Unites States and moved to Ghana"); 
+     System.out.println("when I was four. Now I am back and I go to Lehigh University.");
+     System.out.println("test");
+     
+     //test/testtest
+   }
+}     
+     
+     
+    
+ 
--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld{
     //test/testtest
   }
 }  
-// cd~workspace
+
 
 
     
